3f943bce91d731486fb58cb2e743fc84d73f1433
Win <wins.dominoes2007@gmail.com>
2024-07-05 10:37:05 +0700

    update dependencies

--- lib/constants/info.dart
+++ lib/constants/info.dart
@@ -1,3 +1,3 @@
 const String versionString = "0.0.7";
-const String iconPath = "assets/app_icons/net.winscloud.Datecalculator.svg";
+const String iconPath = "assets/app_icons/net.winscloud.DateCalculator.svg";
 const String iconPathPng = "assets/app_icons/icon.png";
